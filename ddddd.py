e5ad71458f8573dc0103e47973aa9395fb6ba2a7
sallyeojo15 <130116429+sallyeojo15@users.noreply.github.com>
2023-05-10 23:43:37 +0900

    Update ddddd.py
    수정 버전. 대체로 맞지만 중간중간 바뀌지 말아야 할 때 바뀔 때가 있음

--- ddddd.py
+++ ddddd.py
@@ -1,76 +1,72 @@
 import rospy
 import numpy as np
 import cv2
+import signal
+import sys
 
-from sensor_msgs.msg import Image, CompressedImage
-from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
 from std_msgs.msg import Header
+
+from cv_bridge import CvBridge, CvBridgeError
 
 class DetermineColor:
     def __init__(self):
         self.image_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.callback)
         self.color_pub = rospy.Publisher('/rotate_cmd', Header, queue_size=10)
         self.bridge = CvBridge()
-        self.count = 0
-  
+
     def callback(self, data):
         try:
             image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
             msg = Header()
             msg = data.header
             msg.frame_id = '0'
-            img=image
-            img_horizental, img_vertical = img.shape[0], img.shape[1]
-            pixel=[]
-            for i in range(img_horizental):
-                for j in range(img_vertical):
-                    pixel.append(tuple(np.array(img[i,j])*255/(img[i,j][0]+img[i,j][1]+img[i,j][2])))
-    
-            pixel_set=set(pixel)
-            count_list = []
-            for i in pixel_set:
-                count = 0
-                for j in pixel:
-                    if i == j:
-                        count = count + 1
-                count_list.append(count)
+            img = image.copy()
 
-            max_value = max(count_list)
-            max_index = count_list.index(max_value)
+            pixel = img.reshape(-1, 3)  # Reshape image to a 2D array of pixels
+            pixel = pixel.astype(np.float32)  # Normalize pixel values to the range [0, 1]
 
-            background = pixel_set[max_index]
+            unique_colors, color_counts = np.unique(pixel, axis=0, return_counts=True)
+            max_count_index = np.argmax(color_counts)
+            background = unique_colors[max_count_index]
 
-            def get_color_name(b,g,r) :
-                if (b>200) & (g<200) & (r<200) :
-                    color="BLUE"
-                elif (r>200) & (g<200) & (b<200) :
-                    color="RED"
-                else :
-                    color="unknown"
+            def get_color_name(b, g, r):
+                if (b > 200) and (g < 200) and (r < 200):
+                    color = "BLUE"
+                elif (r > 200) and (g < 200) and (b < 200):
+                    color = "RED"
+                else:
+                    color = "unknown"
                 return color
-        
-            background_color=get_color_name(background[0],background[1],background[2])
 
-            if background_color=="BLUE":
-                msg.frame_id='+1'
-            elif background_color=="RED":
-                msg.frame_id='-1'
+            background_color = get_color_name(background[0], background[1], background[2])
+
+            if background_color == "BLUE":
+                msg.frame_id = '+1'
+            elif background_color == "RED":
+                msg.frame_id = '-1'
             else:
-                msg.frame_id='0'
+                msg.frame_id = '0'
 
             self.color_pub.publish(msg)
 
         except CvBridgeError as e:
             print(e)
-        cv2.imshow('Image',image)
+
+        cv2.imshow('Image', image)
         cv2.waitKey(1)
 
-    def rospy_shutdown(self, signal, frame):
-        rospy.signal_shutdown("shut down")
-        sys.exit(0)
+    def shutdown(self):
+        rospy.signal_shutdown("Shutdown")
+        cv2.destroyAllWindows()
 
-      
+
 if __name__ == '__main__':
-    detector = DetermineColor()
     rospy.init_node('CompressedImages1', anonymous=False)
+    detector = DetermineColor()
+
+    signal.signal(signal.SIGINT, detector.shutdown)
+    signal.signal(signal.SIGTERM, detector.shutdown)
+
     rospy.spin()
+
